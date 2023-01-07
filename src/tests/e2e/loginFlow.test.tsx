1510674b92344e181254ee70f63044d41efbb06c
foreign1 <32528727+foreign1@users.noreply.github.com>
2023-01-07 19:08:51 +0100

    #2: write e2e tests

--- src/tests/e2e/loginFlow.test.tsx
+++ src/tests/e2e/loginFlow.test.tsx
@@ -7,10 +7,25 @@ beforeEach(async () => {
   await page.goto('http://localhost:3000/');
 });
 
-describe('super awesome end to end tests', () => {
-  jest.setTimeout(20000);
-
-  test('if it does something', async () => {
-    // ...
+describe('contentscript tests', () => {
+  jest.setTimeout(30000);
+  test('it loads dashboard', async () => {
+    const dashboard = page.$(".dashboardWrapper");
+    expect(dashboard).toBeDefined();
   });
+
+  test('it loads btb button element', async () => {
+    await page.goto('http://localhost:3000/extension');
+    const btb = await page.$(".btb-btn");
+    expect(btb).toHaveTextContent("Budget");
+  });
+
+  test('it displays details element when mouseover btb', async () => {
+    await page.goto('http://localhost:3000/extension');
+    await page.hover(".btb-btn");
+    const details = page.$eval(".details", el => el.innerHTML)
+    const dummyText = document.createElement("p");
+    dummyText.innerText = "Lorem ipsum, dolor sit amet consectetur adiping elit."
+    expect(details).toContainElement(dummyText);
+  })
 });
